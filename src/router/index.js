9cc3e62de141d4fb1e2dbff695d1aad7d943fbda
Laity123 <laity000908@gmail.com>
2020-05-25 12:06:26 +0800

    goods_list module.

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,8 @@ import Rights from '../components/power/Rights'
 import Roles from '../components/power/Roles'
 import Cate from '../components/goods/Cate'
 import Params from '../components/goods/Params'
+import List from '../components/goods/List'
+import Add from '../components/goods/Add'
 
 Vue.use(VueRouter)
 
@@ -38,9 +40,15 @@ const routes = [{
     }, {
       path: '/categories',
       component: Cate
-    },{
-      path:'/params',
+    }, {
+      path: '/params',
       component: Params
+    }, {
+      path: '/goods',
+      component: List
+    }, {
+      path: '/goods/add',
+      component: Add
     }]
   }
 ]
